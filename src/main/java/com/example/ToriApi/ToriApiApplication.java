8ea92831484210315166053ba499705ea0fe3bd9
nursabir <fikus710828@gmail.com>
2023-11-13 21:26:09 +0300

    add password encode

--- src/main/java/com/example/ToriApi/ToriApiApplication.java
+++ src/main/java/com/example/ToriApi/ToriApiApplication.java
@@ -1,6 +1,4 @@
 package com.example.ToriApi;
-
-import com.example.ToriApi.Photo.PhotoMain;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
