fc595e543ba43e25ccc4eb27d8a2db9d648f6506
nursabir <fikus710828@gmail.com>
2023-11-13 21:34:00 +0300

    add password encode

--- src/main/java/com/example/ToriApi/User/PrivacyOptions/UserPrivacyService.java
+++ src/main/java/com/example/ToriApi/User/PrivacyOptions/UserPrivacyService.java
@@ -4,14 +4,19 @@ import com.example.ToriApi.User.DTO.RegisterUserRequestDto;
 import com.example.ToriApi.User.Entityes.User;
 import com.example.ToriApi.User.AdministrationOptions.UserAdministrationController;
 import com.example.ToriApi.User.UserRepository;
+import jakarta.annotation.Resource;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -23,10 +28,14 @@ import java.util.Optional;
 @AllArgsConstructor
 @Getter
 @Setter
+@ComponentScan("ToriSecurity")
 public class UserPrivacyService {
 
     private UserRepository userRepository;
     private final Logger logger = LoggerFactory.getLogger(UserAdministrationController.class);
+
+    //    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     /**
      * @param dto нужен для регистрации юзера
@@ -38,9 +47,11 @@ public class UserPrivacyService {
         if (userRepository.existsUserByLogin(dto.getLogin()))
             return ResponseEntity.status(HttpStatus.CONFLICT).body("We have this login in database: ");
         else {
+            String encodedPassword = passwordEncoder.encode(dto.getPassword());
+
             User createUser = User.builder()
                     .login(dto.getLogin())
-                    .password(dto.getPassword())
+                    .password(encodedPassword)
                     .email(dto.getEmail())
                     .build();
             userRepository.save(createUser);
@@ -52,22 +63,29 @@ public class UserPrivacyService {
         System.out.println("Проверяем соответствие пароля");
         User allowInUser = userRepository.findByLogin(login)
                 .orElseThrow(() -> new ResourceNotFoundException("We dont have this login in database: " + login));
-        if (enteredPassword.equals(allowInUser.getPassword())) {
-            // Пароли совпадают, возвращаем успешный статус со сущностью пользователя
+
+        if (passwordEncoder.matches(enteredPassword, allowInUser.getPassword())) {
             return ResponseEntity.ok(allowInUser);
-        } else return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+        }
+//        if (allowInUser.getLogin().equals(enteredPassword)){
+//            return ResponseEntity.ok(allowInUser);
+//        }
+        // Пароли совпадают, возвращаем успешный статус со сущностью пользователя
+        else return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
     // TODO: 23.10.2023  пока не понимаю что мы тут менять будем.
     // Пока все поменяем кроме внешнего ключа (логин)
-    public ResponseEntity<User> updateUserData(User user) {
+    // парни должны помнить про свой айди как пользователя
+    public ResponseEntity<User> updateUserLogin(Integer id, String login) {
         System.out.println("Обновляем данные о пользователе");
-        Optional<User> currentUser = userRepository.findById(user.getId());
+        Optional<User> currentUser = userRepository.findById(id);
         if (currentUser.isEmpty()) {
             return ResponseEntity.notFound().build();
         } else {
             User existingUser = currentUser.get();
-            existingUser.setLogin(user.getLogin());
+            existingUser.setLogin(login); // поменяли на новый блатной логин
             userRepository.save(existingUser);
             return ResponseEntity.ok(existingUser);
         }
